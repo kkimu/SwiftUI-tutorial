5038fffc91fb46f081d40984bf85e78231f42e4d
kkimu <kimura.kzm@gmail.com>
2019-06-04 08:19:03 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 木邑和馬 on 2019/06/04.
+//  Copyright © 2019 木邑和馬. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
